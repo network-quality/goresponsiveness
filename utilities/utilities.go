e6cd1fedf2522e58a38a51eaa676f59094da8249
Will Hawkins <whh8b@obs.cr>
2022-05-12 15:43:41 -0400

    [Functionality] Change definition of percent change
    Switch from calculating "difference" as percent difference to calculating
    the difference as percent change.

--- utilities/utilities.go
+++ utilities/utilities.go
@@ -32,7 +32,10 @@ func SignedPercentDifference(
 	current float64,
 	previous float64,
 ) (difference float64) {
-	return ((current - previous) / (float64(current+previous) / 2.0)) * float64(
+	//return ((current - previous) / (float64(current+previous) / 2.0)) * float64(
+	//100,
+	//	)
+	return ((current - previous) / previous) * float64(
 		100,
 	)
 }
