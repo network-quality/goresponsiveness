64ed806b94e2d2b2cb4ca14385f7b16042b2f6e7
Will Hawkins <whh8b@obs.cr>
2022-08-10 15:50:45 -0400

    Fix typos in the license text
    There were several places where boilerplate language was left in the
    license block at the top of files containing source.

--- utilities/utilities.go
+++ utilities/utilities.go
@@ -9,7 +9,7 @@
  * PARTICULAR PURPOSE. See the GNU General Public License for more details.
  *
  * You should have received a copy of the GNU General Public License along
- * with Foobar. If not, see <https://www.gnu.org/licenses/>.
+ * with Go Responsiveness. If not, see <https://www.gnu.org/licenses/>.
  */
 
 package utilities
