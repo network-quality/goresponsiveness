fe17152a507bbf94a11cca7f49a51cbae9c0d67b
Will Hawkins <hawkinsw@obs.cr>
2023-06-27 01:23:26 -0400

    Fix broken build -- missing source file.
    Missing source file from commit -- direction.go.

---
+++ direction/direction.go
@@ -0,0 +1,41 @@
+/*
+ * This file is part of Go Responsiveness.
+ *
+ * Go Responsiveness is free software: you can redistribute it and/or modify it under
+ * the terms of the GNU General Public License as published by the Free Software Foundation,
+ * either version 2 of the License, or (at your option) any later version.
+ * Go Responsiveness is distributed in the hope that it will be useful, but WITHOUT ANY
+ * WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR A
+ * PARTICULAR PURPOSE. See the GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License along
+ * with Go Responsiveness. If not, see <https://www.gnu.org/licenses/>.
+ */
+
+package direction
+
+import (
+	"github.com/network-quality/goresponsiveness/datalogger"
+	"github.com/network-quality/goresponsiveness/debug"
+	"github.com/network-quality/goresponsiveness/lgc"
+	"github.com/network-quality/goresponsiveness/probe"
+	"github.com/network-quality/goresponsiveness/rpm"
+)
+
+type Direction struct {
+	DirectionLabel                      string
+	SelfProbeDataLogger                 datalogger.DataLogger[probe.ProbeDataPoint]
+	ForeignProbeDataLogger              datalogger.DataLogger[probe.ProbeDataPoint]
+	ThroughputDataLogger                datalogger.DataLogger[rpm.ThroughputDataPoint]
+	GranularThroughputDataLogger        datalogger.DataLogger[rpm.GranularThroughputDataPoint]
+	CreateLgdc                          func() lgc.LoadGeneratingConnection
+	Lgcc                                lgc.LoadGeneratingConnectionCollection
+	DirectionDebugging                  *debug.DebugWithPrefix
+	ProbeDebugging                      *debug.DebugWithPrefix
+	ThroughputStabilizerDebugging       *debug.DebugWithPrefix
+	ResponsivenessStabilizerDebugging   *debug.DebugWithPrefix
+	LgStabilizationCommunicationChannel chan rpm.ThroughputDataPoint
+	ExtendedStatsEligible               bool
+	StableThroughput                    bool
+	StableResponsiveness                bool
+}
